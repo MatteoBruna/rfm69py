59a9f929d2caeb248c83ce7136082eaf79011fcd
Matteo Bruna <matteo.bruna@gmail.com>
2018-11-22 18:10:04 +0000

    modified example.py to work with node-red flow (added)

--- example.py
+++ example.py
@@ -8,35 +8,38 @@ import struct
 
 try:
     test = RFM69.RFM69(RF69_433MHZ, 1, 100, False)
-    print "class initialized"
-    print "reading all registers"
-    results = test.readAllRegs()
-    for result in results:
-        print result
-    print "Performing rcCalibration"
-    test.rcCalibration()
+    #print "class initialized"
+    #print "reading all registers"
+    #results = test.readAllRegs()
+    #for result in results:
+    #    print result
+    #print "Performing rcCalibration"
+    #test.rcCalibration()
     #print "setting high power"
     #test.setHighPower(True)   #Raspberry pi 3.3V pin cannot supply enough power for this...it's ok as long as you just receive
-    print "Checking temperature"
-    print test.readTemperature(0)
+    #print "Checking temperature"
+    #print test.readTemperature(0)
     #print "setting encryption"
     #test.encrypt("sampleEncryptKey")
-    print "sending blah to 2"
-    if test.sendWithRetry(2, "blah", 3, 20):
-        print "ack recieved"
-    print "reading"
+    #print "sending blah to 2"
+    #if test.sendWithRetry(2, "blah", 3, 20):
+    #    print "ack recieved"
+    #print "reading"
     while True:
         test.receiveBegin()
         while not test.receiveDone():
             time.sleep(.1)
-        print(len(test.DATA)) #print payload size
+        #print(len(test.DATA)) #print payload size
         #print(test.DATA)
         data = test.DATA[:2]+[0x00, 0x00]+test.DATA[2:] #have to do like this...why?
         #data=test.DATA
         id, uptime, temperature, humidity = struct.unpack("hLhh", "".join([chr(x) for x in data]))
         
-        print "id={} uptime={} temperature={} humidity={} from {} RSSI: {}".format(
-            id, uptime, temperature, humidity, test.SENDERID, test.RSSI) #Now temperature and humidity must be divided by 10 
+        #print "id={} uptime={} temperature={} humidity={} from {} RSSI: {}".format(
+         #   id, uptime, temperature, humidity, test.SENDERID, test.RSSI) #Now temperature and humidity must be divided by 10 
+        print "{},{},{},{},{},{}".format(
+            id, uptime, temperature, humidity, test.SENDERID, test.RSSI)#Now temperature and humidity must be divided by 10 
+        
         if test.ACKRequested():
             test.sendACK()
 #execute this code if CTRL + C is used to kill python script
@@ -44,5 +47,6 @@ except KeyboardInterrupt:
   print "You've exited the program"
 
 finally:
-    print "shutting down"
     test.shutdown()
+    #print "shutting down"
+    
